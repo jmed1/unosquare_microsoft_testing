7df339bd477ff739a16cbd3d78b094d37c49d66c
johndoe <johndoe@localhost.localdomain>
2020-10-08 22:34:18 -0500

    Final Working Version POM version.

--- page-objects/microsoft_testing.js
+++ page-objects/microsoft_testing.js
@@ -2,14 +2,93 @@ module.exports = {
 
 url: 'https://www.microsoft.com/en-us/',
 
-elements:{},
+elements:{
+    MenuDiv_selector               : 'nav#uhf-g-nav.c-uhfh-gnav',
+    Office_linkSelector            : 'a#shellmenu_1.c-uhf-nav-link',
+    Windows_linkSelector           : 'a#shellmenu_2.c-uhf-nav-link',
+    Surface_linkSelector           : 'a#shellmenu_3.c-uhf-nav-link',
+    Xbox_linkSelector              : 'a#shellmenu_4.c-uhf-nav-link',
+    Deals_linkSelector             : 'a#shellmenu_5.c-uhf-nav-link',
+    Support_linkSelector           : 'a#l1_support.c-uhf-nav-link',
+    
+    WindowsButton_sel              : 'button#c-shellmenu_52',
+    WindowsElements_sel            : 'ul[data-m*=Windows10_cont] > li > a',
+    WindowsElements_sel_FirstPart  : 'ul[data-m*=Windows10_cont] li:nth-child(',
+    WindowsElements_sel_SecondPart : ') a',
+
+    buttonSearchSelector           : 'button#search.c-glyph',    
+    
+    inputSearchSelector            : 'input#cli_shellHeaderSearchInput',
+    stayInUSAButton                : 'button#R1MarketRedirect-close',
+    price1                         : 'li#coreui-productplacement-30l7ywa_dg7gmgf0dst3 div#coreui-productplacement-30l7ywa_0 span[itemprop=price]',
+    priceItem2                     : 'li#coreui-productplacement-30l7ywa_dg7gmgf0dst4 div#coreui-productplacement-30l7ywa_1 span[itemprop=price]',
+    priceItem3                     : 'li#coreui-productplacement-30l7ywa_dg7gmgf0dst6 div#coreui-productplacement-30l7ywa_2 span[itemprop=price]',
+    firstImage                     : '#coreui-productplacement-30l7ywa_dg7gmgf0dst3 a',
+    price1_second_page             : 'div#ProductPrice_productPrice_PriceContainer.pi-price-text',
+    attributePriceSecondPage       : "aria-label",
+    cancel_signmeup_div            : 'div.c-glyph.glyph-cancel',
+    price1_third_page              : 'div.item-price > div > span.c-price > span[itemprop=price] > span[aria-hidden=true]',
+    buttonAddToCart                : '#buttonPanel_AddToCartButton',
+    selectAmount_selector          : "div.item-quantity > select",
+    select20_selector              : "div.item-quantity > select option:nth-child(20)",
+    finalPrice                     : "div.XsLPvCL_:nth-child(4) > div:nth-child(1) > span:nth-child(1) > span:nth-child(2) > strong:nth-child(1) > span[itemprop=price]"
+
+},
+
+props:{
+    textToBeSought                 : 'Visual Studio',
+    x_price                        : 0,
+    priceValue                     : 'Content',
+    finalPrice_selector_string     : "div.XsLPvCL_:nth-child(4) > div:nth-child(1) > span:nth-child(1) > span:nth-child(2) > strong:nth-child(1) > span[itemprop=price]"
+},
 
 commands:[{
 
+    waitnClick(selector){
+        const page = this;
+        
+        page.waitForElementVisible(selector, 10000, false, ()=>{
+            console.log(`${selector} is visible`)
+            page.click(selector)
+        },`${selector}`)
+
+        return page;
+    }
+    ,
+
+    waitnPrintnStorePrice(selector,attribute){
+        const page = this;
+        
+        page.waitForElementVisible(selector, 10000, false, ()=>{
+            console.log(`${selector} is visible`)
+            
+            page.getAttribute(selector,attribute,(result)=>{
+                console.log(result);
+                page.props.x_price = parseInt(result.value.replace(/Current price \$|\$|\,/g,""),10);
+                console.log("Price = " + page.props.x_price);
+            })    
+
+        },`${selector}`)
+
+        return page;
+    }
+    ,
+
+    validatePricesAreEqual(price_A, price_B){
+        
+        const page = this;
+        
+        if (price_A === price_B){
+            console.log(`Comparing prices ${price_A} and ${price_B}`)
+            page.api.verify.ok(true, "Prices are equal.");    
+        }
+        else{
+            console.log(`Comparing prices ${price_A} and ${price_B}`)
+            page.api.verify.ok(false,"Precios are diferentes");
+        }
+
+        return page;
+    }
 }]
-
-
-
-
 
 }
--- tests/unosquare_microsoft.js
+++ tests/unosquare_microsoft.js
@@ -1,109 +1,68 @@
 /*José F. Medrano, Sep, 23, 2020*/
-const verificar = require('assert')
+//chai = require('chai')
+
+const verificar = require('assert');
 
 module.exports = {
     url: 'https://google.com',
     
     '@tags':['unosquare_microsoft_testing'],
 
-    /*waitForCall:async function(browser){
-        
-        for (i = 0; i > 3; i++) {
-            result  =  await browser.getText('css selector', 
-                'ul[data-m*=Windows10_cont]:nthchild('+i+')')
-            
-            console.log("li = " + result.value)
-        }
-
-    }*/
-
-
-
-
-
-    /*demoTestAsync: async function(browser) {
-        await browser.init();
-        const text = await browser.getText("#main ul li", function(result) {
-          return Promise.resolve(resolve.value);
-        });
-        console.log('text', text);
-      }
-
-    ,*/    
-
     'Unosquare_Microsoft_test'(browser){
-        
-        const buttonSearchSelector = 'button#search.c-glyph';
-        const inputSearchSelector = 'input#cli_shellHeaderSearchInput';                                               
-        const textToBeSought = "Visual Studio";
-        const stayInUSAButton = 'button#R1MarketRedirect-close';
-        const price1 =       'li#coreui-productplacement-30l7ywa_dg7gmgf0dst3 div#coreui-productplacement-30l7ywa_0 span[itemprop=price]'
-        const price_other2 = 'li#coreui-productplacement-30l7ywa_dg7gmgf0dst4 div#coreui-productplacement-30l7ywa_1 span[itemprop=price]'
-        const price_other3 = 'li#coreui-productplacement-30l7ywa_dg7gmgf0dst6 div#coreui-productplacement-30l7ywa_2 span[itemprop=price]'
-        
+                
         const priceValue = 'content'
         price_saved = 0;
         price1_val2 = 0;
         price1_val3 = 0;
-        const firstImage = '#coreui-productplacement-30l7ywa_dg7gmgf0dst3 a'
-        const price1_second_page = 'div#ProductPrice_productPrice_PriceContainer.pi-price-text';
         const attributePriceSecondPage = "aria-label"
-        //const price1_second_page = 'div#ProductPrice_productPrice_PriceContainer.pi-price-text span[aria-disabled=false]';
-        //const price1_second_page = '/html/body/section/div[1]/div[1]/div[1]/div[2]/div[6]/div/div[1]/div/div/div[1]/span';                          
-        const cancel_signmeup_div = 'div.c-glyph.glyph-cancel';        
-        const price1_third_page = 'div.item-price > div > span.c-price > span[itemprop=price] > span[aria-hidden=true]';
-        //const price1_third_page = '.item-price > div:nth-child(1) > span:nth-child(1) > span:nth-child(3) > span:nth-child(1)';
-        const buttonAddToCart = '#buttonPanel_AddToCartButton';
-
-        const selectAmount_selector = "div.item-quantity > select";
-        const select20_selector = "div.item-quantity > select option:nth-child(20)";
-        //const finalPrice = "span[itemprop=price]"
-        //const finalPrice = "div.XsLPvCL_:nth-child(4) > div:nth-child(1) > span:nth-child(1) > span:nth-child(2) > strong:nth-child(1) > span:nth-child(1)"
         const finalPrice = "div.XsLPvCL_:nth-child(4) > div:nth-child(1) > span:nth-child(1) > span:nth-child(2) > strong:nth-child(1) > span[itemprop=price]"
         total = "";
-
-
         num_renglones = 0;
-        
+
+        const page = browser.page.microsoft_testing();
+        page
+        .perform(()=>{console.log('/*1. Go to https://www.microsoft.com/en-us/*/')})
         /*1. Go to https://www.microsoft.com/en-us/*/
-        browser.timeoutsImplicitWait(40000, function(result) {
-            console.log('Setting timeoutsImplicitWait to 40000');})
-        .url('https://www.microsoft.com/en-us/')
-        .waitForElementVisible('nav#uhf-g-nav.c-uhfh-gnav')
+        
+        .navigate()
+
+        .perform(()=>{console.log('/*2. Validate all menu items are present (Office - Windows - Surface - Xbox - Deals - Support)*/')})
         /*2. Validate all menu items are present (Office - Windows - Surface - Xbox - Deals - Support)*/
-        .verify.containsText("a#shellmenu_1.c-uhf-nav-link","Office", "Office link present")
-        .verify.containsText("a#shellmenu_2.c-uhf-nav-link","Windows", "Windows link present")
-        .verify.containsText("a#shellmenu_3.c-uhf-nav-link","Surface")
-        .verify.containsText("a#shellmenu_4.c-uhf-nav-link","Xbox", "Xbox link present")
-        .verify.containsText("a#shellmenu_5.c-uhf-nav-link","Deals", "Deals link present")
-        .verify.containsText("a#l1_support.c-uhf-nav-link","Support", "Support link present")
+        
+        .waitForElementVisible('@MenuDiv_selector',2000,false)
+        .verify.containsText('@Office_linkSelector',"Office", "Office link present")
+        .verify.containsText('@Windows_linkSelector',"Windows", "Windows link present")
+        .verify.containsText('@Surface_linkSelector',"Surface", "Surface link present")
+        .verify.containsText('@Xbox_linkSelector',"Xbox", "Xbox link present")
+        .verify.containsText('@Deals_linkSelector',"Deals", "Deals link present")
+        .verify.containsText('@Support_linkSelector',"Support", "Support link present")
+
+        .perform(()=>{console.log('/*3. Go to Windows (Trying to click on the Windows link)*/')})
         /*3. Go to Windows (Trying to click on the Windows link)*/
-        .click('a#shellmenu_2.c-uhf-nav-link')
+        
+        .click('@Windows_linkSelector')
+        
+        .perform(()=>{console.log('/*4. Once in Windows page, click on Windows 10 Menu*/')})
         /*4. Once in Windows page, click on Windows 10 Menu*/
+        
         .assert.urlEquals('https://www.microsoft.com/en-us/windows/', "Windows Link to Windows product webpage")
-        /*Windows 10 Menu*/
-        .waitForElementVisible('button#c-shellmenu_52')
+        /*Windows 10 Menu*/                
+        //.waitForElementVisible('@WindowsButton_sel', 10000, false, 'Windows Button Dropdown is visible')
         /*Clicking on Windows 10 Menu dropdown list*/
-        .click('button#c-shellmenu_52')
+        .verify.visible('@WindowsButton_sel', 'Windows Button Dropdown is visible')
+        .click('@WindowsButton_sel')
+
+        .perform(()=>{console.log('/* 5. Print all Elements in the dropdown*/')})
         /* 5. Print all Elements in the dropdown*/
         
-
-
-        browser.elements('css selector','ul[data-m*=Windows10_cont] > li > a', function(result) {
+        //browser.elements('css selector','@WindowsElements_sel', function(result) {
+        page.api.elements('css selector','ul[data-m*=Windows10_cont] > li > a', function(result) {
                 
             console.log("Printing all Elements in Windows 10 dropdown: ")
-
-            result.value.forEach(function(value){
-                
-                num_renglones +=1;
-                //result2 = JSON.stringify(value)
-                //result2 = result2.trim();
-                //result2 = result2.replace(/{|}/g,"")
-                //console.log("result2 >" + result2)
-                /*result_text = browser.elementIdText(result2)
-                console.log("result_text = "+ result_text)*/
-
-                browser.getText('css selector', 
+            
+            result.value.forEach(function(value){    
+                num_renglones +=1;                                
+                page.getText('css selector', 
                 'ul[data-m*=Windows10_cont] li:nth-child('+(num_renglones)+') a',
                 (a_s)=>{
                     console.log(JSON.stringify(a_s.value).trim().replace(/{|}/g,""))
@@ -111,173 +70,124 @@ module.exports = {
             })
         })
 
-
-
-        /*   result.value.forEach(function (value) {
-              elementID = value;
-              console.log('Checking Element - ' + elementID);
-            });
-          })*/
-
-          
-        /*browser.elementIdText(elementID , function(result) {
-            
-            console.log("dfdf:" + result)
-            console.log("dfdf:" + result.value)
-
-        })
-
-        browser.getText('css selector', 'ul[data-m*=Windows10_cont] li', function(result) {
-            
-            console.log(result.value)
-
-        })*/
-
-        /*.getText('ul[data-m*=Windows10_cont] li', function(result) {
-            return new Promise(function(resolve, reject) {
-              setTimeout(function() {
-                console.log('Value:', result.value);
-                resolve();
-              }, 1000);
-            });
-          })*/
-
-        /*.perform(function(){
-            var list = document.getElementsByTagName('ul[data-m*=Windows10_cont] li');
-            console.log(list);
-        })*/
-        
-
-
+        .perform(()=>{console.log('/*6.Go to Search next to shopping cart*/')})
         /*6.Go to Search next to shopping cart*/
-        .waitForElementVisible(buttonSearchSelector)
-        .click(buttonSearchSelector)
-        .waitForElementVisible(inputSearchSelector)
+        
+        page.waitnClick('@buttonSearchSelector')
+        .waitForElementVisible('@inputSearchSelector')
+        
+        .perform(()=>{console.log('/*7. Search for Visual Studio */')})
         /*7. Search for Visual Studio */
-        .setValue(inputSearchSelector, textToBeSought)
-        .click(buttonSearchSelector)
-        .waitForElementVisible(stayInUSAButton,10000,false)
-        .click(stayInUSAButton)
+        
+        .setValue('@inputSearchSelector', page.props.textToBeSought)
+        .click('@buttonSearchSelector')
+        /*Dismissing stay in USA pop up div.*/
+        .waitnClick('@stayInUSAButton')
+        
+        .perform(()=>{console.log('/*8. Print the price for the 3 first elements listed in Software result list*/')})
         /*8. Print the price for the 3 first elements listed in Software result list*/
+        
+        .waitnPrintnStorePrice('@price1', page.props.priceValue)
+        
+        .perform(()=>{console.log('/*9. Store the price of the first one*/')})
         /*9. Store the price of the first one*/
-        .waitForElementVisible(price1)
-        .getAttribute(price1, priceValue, (result)=>{
-            console.log(result);
-            price_saved= parseInt(result.value.slice(1),10);
-            console.log("Price = "+price_saved);
-        })
-        .waitForElementVisible(price_other2)
-        .getAttribute(price_other2, priceValue, (result)=>{
-            console.log(result);
-            price_saved= parseInt(result.value.slice(1),10);
-            console.log("Price = "+price_saved);
-        })
-        .waitForElementVisible(price_other3)
-        .getAttribute(price_other3, priceValue, (result)=>{
-            console.log(result);
-            price_saved= parseInt(result.value.slice(1),10);
-            console.log("Price = "+price_saved);
-        })
-        /* 10. Click on the first one to go to the details page */
-        .click(firstImage)
-        /* 11.Once in the details page, validate both prices are the same */
-        .waitForElementVisible(price1_second_page)
-        //.getText("xpath",price1_second_page, result=>{
-        .getAttribute(price1_second_page, attributePriceSecondPage, (result2)=>{      
-        //.getText(price1_second_page, result2=>{
-            console.log("Price s2 = "+ JSON.stringify(result2));
-            price1_val2 = parseInt(result2.value.replace(/Current price \$/g,""),10);
-            price1_val2 = parseInt(price1_val2,10);
-            console.log("Price 2 = "+ price1_val2);
-        })
-        //.assert.ok(price_saved === price1_val2, "Precios "+price_saved+" iguales")
+        
+        /*The value must be saved immediately after being retrieved by waitnPrintnStorePrice 
+        otherwise it is lost on the next call of the function*/
         .perform(()=>{
-            if (price_saved === price1_val2) 
-            console.log("Precios "+price_saved +" iguales");
+            //Save stored price.
+            price_saved = page.props.x_price
+            console.log("price_saved = " + price_saved)
         })
+
+        .perform(()=>{console.log('/*8. Print the price for the 3 first elements listed in Software result list*/')})
+        /*8. Print the price for the 3 first elements listed in Software result list*/
+        
+        .waitnPrintnStorePrice('@priceItem2', page.props.priceValue)
+        .waitnPrintnStorePrice('@priceItem3', page.props.priceValue)
+        
+        .perform(()=>{console.log('/* 10. Click on the first one to go to the details page */')})
+        /* 10. Click on the first one to go to the details page */
+        
+        .click('@firstImage')
+        
+        .perform(()=>{console.log('/* 11.Once in the details page, validate both prices are the same */')})
+        /* 11.Once in the details page, validate both prices are the same */        
+        
+        .waitnPrintnStorePrice('@price1_second_page',attributePriceSecondPage)
+        .perform(()=>{
+            //Saving last price obtained.
+            price1_val2 = page.props.x_price
+            //console.log("price1_val2 ="+price1_val2)
+        })
+        
+        .perform(()=>{
+            //Perform the following statements synchronously
+            console.log('Comparing prices')
+            page.validatePricesAreEqual(price_saved,price1_val2)
+        })
+        
+        .perform(()=>{console.log('/*12. Click Add to Cart*/')})
         /*12. Click Add to Cart*/
-        .waitForElementVisible(buttonAddToCart)
-        .click(buttonAddToCart)
+        
+        .waitnClick('@buttonAddToCart')
         .pause(5000)
-        .waitForElementVisible(cancel_signmeup_div,10000,false)
-        .verify.visible(cancel_signmeup_div, "Sign me up pop up div present")
-        .click(cancel_signmeup_div)
-        .verify.not.visible(cancel_signmeup_div, "Sign me up pop up div is NOT present anymore.")
-        .verify.visible(buttonAddToCart, "Add to cart button was still present")
-        .click(buttonAddToCart)
-        //.verify.not.visible(buttonAddToCart, "Add to cart button was NOT present anymore.")
+        .verify.visible('@cancel_signmeup_div', "Sign me up pop up div present")
+        .waitnClick('@cancel_signmeup_div')
+        .verify.not.visible('@cancel_signmeup_div', "Sign me up pop up div is NOT present anymore.")
+        .verify.visible('@buttonAddToCart', "Add to cart button was still present")
+        .waitnClick('@buttonAddToCart')
+        
+        .perform(()=>{console.log('/*13. Verify all 3 price amounts are the same.*/')})
         /*13. Verify all 3 price amounts are the same.*/
-        .waitForElementVisible(price1_third_page)
-        .getText(price1_third_page, result3=>{
+        
+        .waitForElementVisible('@price1_third_page',10000,false)
+        .getText('@price1_third_page', result3=>{
             price1_val3 = parseInt(result3.value.slice(1),10);
             console.log("Price3 = "+price1_val3);
-            if ( price1_val3 === price1_val2 ) 
-            console.log("Precios "+price_saved +" iguales");
         })
-        /*14. On the # of items dropdown select 20 and validate the Total amount is Unit Price * 20*/
-        .waitForElementVisible(selectAmount_selector)
-        .click(selectAmount_selector)
-        .waitForElementVisible(select20_selector)
-        .click(select20_selector)
-        .pause(5000)
-        .waitForElementVisible(finalPrice, 10000, false)
+        .perform(()=>{
+            //Perform the following statements synchronously
+            console.log("Verifying all 3 prices amounts are the same.");
+            page.validatePricesAreEqual(price_saved,price1_val2)
+            page.validatePricesAreEqual(price1_val2,price1_val3)
+        })
 
-        browser.execute(function (finalPrice) {
-            var innerTotal = 0;
-            //a = document.querySelector("div.XsLPvCL_:nth-child(4) > div:nth-child(1) > span:nth-child(1) > span:nth-child(2) > strong:nth-child(1) > span[itemprop=price]")
+        .perform(()=>{console.log('/*14. On the # of items dropdown select 20 and validate the Total amount is Unit Price * 20*/')})
+        /*14. On the # of items dropdown select 20 and validate the Total amount is Unit Price * 20*/
+        
+        .waitnClick('@selectAmount_selector')
+        .waitnClick('@select20_selector')
+        .pause(5000)
+        .waitForElementVisible('@finalPrice', 10000, false)
+
+        //page.api.execute(function (final_price_string) {
+        page.api.execute(function (finalPrice) {
+            let innerTotal = 0;
             a = document.querySelector(finalPrice)
-               //.forEach(a => innerTotal += Number(a.innerText));
-               //.forEach(a =>{
-                //innerTotal += Number(a.innerText)
-               //});
             return a.innerText;
-            //return innerTotal;
-          //}, [], function(res) {
           }, [finalPrice], function(res) {
             total = Number(JSON.stringify(res.value).replace(/,|\"|\$|}/gi,"").trim());
-            console.log("res -> "+ JSON.stringify(res))
+            /*console.log("res -> "+ JSON.stringify(res))
             console.log("TOTAL = "+total)
-            console.log("type of TOTAL = "+ typeof(total) )
+            console.log("type of TOTAL = "+ typeof(total) )*/
         })
 
         .perform(()=>{
             console.log(total)
-            if (total === 20*price1_val3)
+            const unit_price = price1_val3;
+            const num_of_purchased_units = 20
+            page.validatePricesAreEqual( total, (num_of_purchased_units*unit_price) )
+
+            if (total === num_of_purchased_units*unit_price)
             {
                 console.log("Total amount equals sum of items' price.")
                 console.log("Total Amount = " + total)
-                verificar.equal(total,(20*price1_val3))
             }
             else{
                 console.log("Final price = " + JSON.stringify(total))
-                verificar.equal(total,(20*price1_val3),"LA SUMA DE LOS ARTÍCULOS ES INCORRECTA.")
+                console.log("Incorrect Total SUM")
             }
-
         })
     }
-    ,
-
-
-    
-    'prueba': function (browser) {
-        /*var google = browser.page.google();*/
-        //nightwatch
-          //url.navigate()
-          /*.assert.titleContains('Nightwatch.js');*/
-   
-        //nightwatch.api.elements('ul li', function(result) {
-        browser
-        .url('https://www.microsoft.com/en-us/windows')
-        .elements('css selector','ul li')
-        .getText('css selector', 'ul li')        
-        /*.elements('css selector','ul li', function(result) {
-          console.log(result);
-        });*/
-   
-        browser.end();
-     }
-
 }
-
-//#coreui-productplacement-30l7ywa_0 > div:nth-child(3) > div:nth-child(1) > span:nth-child(3) > span:nth-child(1)
-//#coreui-productplacement-30l7ywa_1 > div:nth-child(3) > div:nth-child(1) > span:nth-child(3) > span:nth-child(1)
